da9e49ad3781918832ab628a2a193f30f76f2ebd
yigit.olmez <yigit.olmez@argedor.com>
2025-12-29 10:39:18 +0300

    feat: enable release signing by loading keystore properties from `key.properties` and applying them to the release build type.

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -8,7 +8,11 @@ plugins {
     id("dev.flutter.flutter-gradle-plugin")
 }
 
-
+val keystoreProperties = Properties()
+val keystorePropertiesFile = rootProject.file("key.properties")
+if (keystorePropertiesFile.exists()) {
+    keystoreProperties.load(FileInputStream(keystorePropertiesFile))
+}
 
 android {
     namespace = "com.ginfit.ai.outfit.generator.fitcheck"
@@ -38,16 +42,16 @@ android {
 
     signingConfigs {
         create("release") {
-            // keyAlias = keystoreProperties["keyAlias"] as String?
-            // keyPassword = keystoreProperties["keyPassword"] as String?
-            // storeFile = file(keystoreProperties["storeFile"] as String)
-            // storePassword = keystoreProperties["storePassword"] as String?
+            keyAlias = keystoreProperties["keyAlias"] as String?
+            keyPassword = keystoreProperties["keyPassword"] as String?
+            storeFile = file(keystoreProperties["storeFile"] as String)
+            storePassword = keystoreProperties["storePassword"] as String?
         }
     }
 
     buildTypes {
         getByName("release") {
-            // signingConfig = signingConfigs.getByName("release")
+            signingConfig = signingConfigs.getByName("release")
             isMinifyEnabled = true
             proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
         }
